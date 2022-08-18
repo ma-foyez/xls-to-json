f1adb0f5c9782a0f069077bf1b2154e4bf120a06
ma-foyez <mafoyez.bd@gmail.com>
2022-08-18 22:33:49 +0600

    json formate & validation add

--- excel.js
+++ excel.js
@@ -1,71 +1,82 @@
-var input = document.getElementById("input");
-//data format
-var jsonFormattedData = {
-    EmployeeID: "",
-    OfficeID: "",
-    NameOfEmployee: "",
-    bulkDutyDetail: [],
-    // bulkDutyDetail: [{
-    //     DayNumber: 0,
-    //     ShiftName: ""
-    // }]
-};
+//Model Class for Duty Roster Master
+class dutyRosterMaster {
+  constructor(EmployeeID, OfficeID, NameOfEmployee, bulkDutyDetail) {
+    this.EmployeeID = EmployeeID;
+    this.OfficeID = OfficeID;
+    this.NameOfEmployee = NameOfEmployee;
+    this.bulkDutyDetail = [];
+  }
+}
+
+//Model Class for Duty Roster Details
+class dutyRosterDetails {
+  constructor(DayNumber, ShiftName) {
+    this.DayNumber = DayNumber;
+    this.ShiftName = ShiftName;
+  }
+}
 
 input.addEventListener("change", function () {
-    readXlsxFile(input.files[0]).then(function (data) {
-        var i = 0;
-        var headers = [];
-        var json_object = [];
-        data.map((row, index) => {
-            if (i === 0) {
-                headers = row;
-            }
-            if (i > 0) {
-                var temp = {};
-                const rowArrayValue = row.slice(3, 60);
+  var input = document.getElementById("input");
+  readXlsxFile(input.files[0]).then(function (data) {
+    var i = 0;
+    var headers = [];
+    var json_object = [];
+    var masterDutyRoster = [];
+    data.map((row, index) => {
+      if (i === 0) {
+        headers = row;
+      }
+      if (i > 0) {
+        var temp = {};
+        for (let j = 0; j < row.length; j++) {
+          temp[headers[j]] = row[j];
+        }
 
-                for (let j = 0; j < row.length; j++) {
-                    temp[headers[j]] = row[j];
-                    // insert data into formatted json data set
-                    jsonFormattedData.EmployeeID = temp.EmployeeID;
-                    jsonFormattedData.NameOfEmployee = temp.NameOfEmployee;
-                    jsonFormattedData.OfficeID = temp.OfficeID;
-
-                    const newValue = Object.keys(temp).reduce(function (obj, key) {
-                        if (key != "EmployeeID" && key != "NameOfEmployee" && key != "OfficeID") {           //key you want to remove
-                            obj[key] = temp[key];
-                        }
-                        return obj;
-                    }, {});
-
-                }
-
-                console.log('temp :>> ', temp);
-
-
-
-                var details = {
-                    ShiftName: '',
-                    DayNumber: '',
-                }
-                for (let index = 0; index < newValue.length; index++) {
-                    const element = newValue[index];
-                    console.log('element :>> ', element);
-                    details.ShiftName = element;
-                    jsonFormattedData.bulkDutyDetail.push(details);
-
-                }
-
-                // find data from header row
-                // for (let x = 0; x < headers.length; x++) {
-                //     const xyz = headers[x];
-                //     console.log('xyz value is :>> ', xyz);
-                // }
-                json_object.push(temp);
-            }
-            i++;
-        });
-
-        document.getElementById("json-data").value = JSON.stringify(json_object, null, 4);
+        json_object.push(temp);
+      }
+      i++;
     });
+
+    for (let k = 0; k < json_object.length; k++) {
+      // per roster object
+      const singleDutyRosterObject = json_object[k];
+      const dutyRoster = new dutyRosterMaster();
+      dutyRoster.EmployeeID = singleDutyRosterObject["EmployeeID"];
+      dutyRoster.OfficeID = singleDutyRosterObject["OfficeID"];
+      dutyRoster.NameOfEmployee = singleDutyRosterObject["NameOfEmployee"];
+
+      const findDutyDetailsValue = Object.keys(singleDutyRosterObject).reduce(
+        function (obj, key) {
+          if (
+            key != "EmployeeID" &&
+            key != "NameOfEmployee" &&
+            key != "OfficeID"
+          ) {
+            //remove by keys Ex: EmployeeID, OfficeID, NameOfEmployee
+            obj[key] = singleDutyRosterObject[key];
+          }
+          return obj;
+        },
+        {}
+      );
+      // get object keys for reassign value
+      var objKeys = Object.keys(findDutyDetailsValue);
+      for (let l = 0; l < objKeys.length; l++) {
+        const objDayNumber = objKeys[l];
+        const objShiftName = singleDutyRosterObject[objDayNumber];
+
+        var dutyDetails = new dutyRosterDetails();
+        dutyDetails.DayNumber = objDayNumber;
+        dutyDetails.ShiftName = objShiftName;
+        dutyRoster.bulkDutyDetail.push(dutyDetails);
+      }
+      masterDutyRoster.push(dutyRoster);
+    }
+    document.getElementById("json-data").value = JSON.stringify(
+      masterDutyRoster,
+      null,
+      4
+    );
+  });
 });
